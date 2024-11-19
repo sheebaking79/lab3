c86810a002090ff025fd43761f3c58613c2e9660
sheebaking79 <sheebaking79@gmail.com>
2024-11-19 21:14:56 +0530

    Initial commit

---
+++ bmi.py
@@ -0,0 +1,15 @@
+# def calculate_bmi(height, weight):
+#  print("Height = " + height)
+#  print("Weight = " + weight)
+# calculate_bmi(weight="57", height="1.73")
+
+
+# def calculate_bmi(height, weight):
+#  print("Height = " + height)
+#  print("Weight = " + weight)
+# calculate_bmi(weight=57, height=1.73)
+
+def calculate_bmi(height, weight):
+ print("Height = " + str(height))
+ print("Weight = " + str(weight))
+calculate_bmi(weight=57, height=1.73)
---
+++ test_lab3.py
@@ -0,0 +1,49 @@
+
+import Lab3
+
+print("Test_Lab3")
+
+
+def test_bubble_sort_ascending():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    test_arr = [11, 12, 22, 25, 34, 64, 90]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == test_arr)
+
+def test_bubble_sort_descending():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    test_arr = [90, 64, 34, 25, 22, 12, 11]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == test_arr)
+
+def test_bubble_sort_more_ten():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 22, 11, 90, 12]
+    result = Lab3.bubble_sort(input_arr,1)
+
+    assert (result == 1)
+
+def test_bubble_sort_equals_zero():
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr,1)
+
+    assert (result == 0)
+
+def test_bubble_sort_not_integer():
+    input_arr = [3, 1, 'a', 5, 2]
+    result = Lab3.bubble_sort(input_arr,1)
+
+    assert (result == 2)
+
+def test_bubble_sort_invalid():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+
+    result = Lab3.bubble_sort(input_arr, 3)
+
+    assert (result == [])
